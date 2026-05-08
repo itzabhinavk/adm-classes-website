7f3a6c9ab54c5cdd4f3248b6ac4c60c42878edf3
Abhinav Kumar <abhinavk813101@gmail.com>
2026-05-08 08:19:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,9 @@ db_config = {
     "host": os.environ.get('DB_HOST', 'localhost'),      # MySQL server host
     "user": os.environ.get('DB_USER', 'root'),           # MySQL username
     "password": os.environ.get('DB_PASSWORD', ''),       # MySQL password
-    "database": os.environ.get('DB_NAME', '')         # Database name
+    "database": os.environ.get('DB_NAME', 'adm')         # Database name
+    "port": int(os.environ.get('DB_PORT', 3306)),        # MySQL port
+
 }
 
 # OAuth Configuration (Google & GitHub)
